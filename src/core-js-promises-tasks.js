b48054e95fe54996199174f0b17cac751974915b
yauhenibelski <108809026+yauhenibelski@users.noreply.github.com>
2024-04-15 21:46:26 +0200

    queuPromises

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -128,8 +128,14 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuPromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuPromises(promises) {
+  return promises.reverse().reduce(
+    (acc, promise) =>
+      acc.then((accResult) => {
+        return promise.then((promResult) => promResult + accResult);
+      }),
+    Promise.resolve('')
+  );
 }
 
 module.exports = {
