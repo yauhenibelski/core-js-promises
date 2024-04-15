c3fa176ce5e35d67d123567e18a470a222a7bcd6
yauhenibelski <108809026+yauhenibelski@users.noreply.github.com>
2024-04-15 20:22:33 +0200

    getFirstResolvedPromiseResult

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -50,8 +50,8 @@ function getPromiseResult(source) {
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with 1
  * [Promise.reject(1), Promise.reject(2), Promise.reject(3)]    => Promise rejected
  */
-function getFirstResolvedPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+function getFirstResolvedPromiseResult(promises) {
+  return Promise.any(promises);
 }
 
 /**
